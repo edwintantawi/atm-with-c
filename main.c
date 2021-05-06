33feac6122bb71629dac81686e384eeb72518790
edwintantawi <edwintantawi@gmail.com>
2021-05-06 23:10:15 +0700

    added logout feature

--- main.c
+++ main.c
@@ -40,201 +40,188 @@ void main()
   int inputMenu;
   char inputIdCard[10];
   char inputPassword[10];
+  int isLogout = 0;
   // end | initial value
+  do{
+    // start | user input
+    printf("idCard\t: ");
+    scanf("%s", &inputIdCard);
+    printf("Password: ");
+    scanf("%s", &inputPassword);
+    // end | user input
 
-  // start | user input
-  printf("idCard\t: ");
-  gets(inputIdCard);
-  printf("Password: ");
-  gets(inputPassword);
-  // end | user input
-
-  // start | check user idCard and password
-  for(int i=0; i<2; i++){
-    if(strcmp(user[i].idCard, inputIdCard) == 0 && strcmp(user[i].password, inputPassword) == 0){
-      activeUser = i;
+    // start | check user idCard and password
+    for(int i=0; i<2; i++){
+      if(strcmp(user[i].idCard, inputIdCard) == 0 && strcmp(user[i].password, inputPassword) == 0){
+        activeUser = i;
+      }
     }
-  }
-  // end | check user idCard and password
+    // end | check user idCard and password
 
-  // start | show status user
-  if(activeUser != -1){
-      int menu = 1;
-      while (menu == 1){
-        system("clear");
-        printf("ID Card\t: %s \n", user[activeUser].idCard);
+    // start | show status user
+    if(activeUser != -1){
+        int menu = 1;
+        while (menu == 1){
+          system("clear");
+          printf("ID Card\t: %s \n", user[activeUser].idCard);
 
-        // menu
-        printf("Menu (select by number) :\n");
-        printf("1. Balance check\n");
-        printf("2. Transfer\n");
-        printf("3. Transaction history\n\n");
-        printf("Selection (1 or 2 or 3) : ");
-        scanf("%d", &inputMenu);
+          // menu
+          printf("Menu (select by number) :\n");
+          printf("1. Balance check\n");
+          printf("2. Transfer\n");
+          printf("3. Transaction history\n");
+          printf("0. Logout\n\n");
+          printf("Selection (1 or 2 or 3 or 0) : ");
+          scanf("%d", &inputMenu);
 
-        switch (inputMenu){
-          case 1:
-            system("clear");
-            printf("ID Card\t: %s \n", user[activeUser].idCard);
-            printf("Balance\t: $%d \n\n", user[activeUser].balance);
-            printf("1. Back\n");
-            printf("0. Close & logout\n");
-            printf("Selection (1 or 0) : ");
-            scanf("%d", &inputMenu);
+          switch (inputMenu){
+            case 1:
+              system("clear");
+              printf("ID Card\t: %s \n", user[activeUser].idCard);
+              printf("Balance\t: $%d \n\n", user[activeUser].balance);
+              printf("1. Back\n");
+              printf("Selection (1) : ");
+              scanf("%d", &inputMenu);
 
-            switch (inputMenu){
-              case 1:
-                menu = 1;
-                break;
-              case 0:
-                menu = 0;
-                system("clear");
-                break;
-              default:
-                printf("Invalid Input!");
-                break;
-            }
-            break;
-          case 2:
-            // transfer
-            system("clear");
-            printf("ID Card\t: %s \n", user[activeUser].idCard);
-            printf("Balance\t: $%d \n\n", user[activeUser].balance);
+              switch (inputMenu){
+                case 1:
+                  menu = 1;
+                  break;
+                default:
+                  printf("Invalid Input!\n");
+                  break;
+              }
+              break;
+            case 2:
+              // transfer
+              system("clear");
+              printf("ID Card\t: %s \n", user[activeUser].idCard);
+              printf("Balance\t: $%d \n\n", user[activeUser].balance);
 
-            char transferTo[5];
-            int transferNominal;
-            printf("Transfer To (ID Card) : ");
-            scanf("%s", &transferTo);
-            printf("Nominal ($100 - $%d) : ", user[activeUser].balance);
-            scanf("%d", &transferNominal);
-            puts("");
-            printf("1. Back\n");
-            printf("0. Close & logout\n");
-            printf("9. Transaction confirmation\n");
-            printf("Selection (1 or 0 or 9) : ");
-            scanf("%d", &inputMenu);
+              char transferTo[5];
+              int transferNominal;
+              printf("Transfer To (ID Card) : ");
+              scanf("%s", &transferTo);
+              printf("Nominal ($100 - $%d) : ", user[activeUser].balance);
+              scanf("%d", &transferNominal);
+              puts("");
+              printf("1. Back\n");
+              printf("9. Transaction confirmation\n");
+              printf("Selection (1 or 9) : ");
+              scanf("%d", &inputMenu);
 
-            switch (inputMenu){
-              case 1:
-                menu = 1;
-                break;
-              case 0:
-                menu = 0;
-                system("clear");
-                break;
-              case 9:
-                if(transferNominal >= 100 && transferNominal <= user[activeUser].balance){
-                  system("clear");
+              switch (inputMenu){
+                case 1:
+                  menu = 1;
+                  break;
+                case 9:
+                  if(transferNominal >= 100 && transferNominal <= user[activeUser].balance){
+                    system("clear");
 
-                  // TODO process transaction
-                  // added transaction
-                  strcpy(transactionHistory[transactionHistoryLabel].from, user[activeUser].idCard);
-                  strcpy(transactionHistory[transactionHistoryLabel].to, transferTo);
+                    // TODO process transaction
+                    // added transaction
+                    strcpy(transactionHistory[transactionHistoryLabel].from, user[activeUser].idCard);
+                    strcpy(transactionHistory[transactionHistoryLabel].to, transferTo);
 
-                  int transferToId = -1;
-                  for (int i = 0; i < 2; i++){
-                    if(strcmp(user[i].idCard, transferTo) == 0){
-                      transferToId = i;
+                    int transferToId = -1;
+                    for (int i = 0; i < 2; i++){
+                      if(strcmp(user[i].idCard, transferTo) == 0){
+                        transferToId = i;
+                      }
+                    }
+
+                    if(transferToId != -1){
+                      transactionHistory[transactionHistoryLabel].nominal = transferNominal;
+                      user[activeUser].balance -= transferNominal;
+                      user[transferToId].balance += transferNominal;
+                      transactionHistoryLabel++;
+                      printf("Transfer Success\n\n");
+                    } else {
+                      printf("Transfer Failed, Invalid Target Card Id\n\n");
+                    }
+
+                    printf("1. Back\n");
+                    printf("Selection (1) : ");
+                    scanf("%d", &inputMenu);
+                    switch (inputMenu){
+                      case 1:
+                        menu = 1;
+                        break;
+                      default:
+                        printf("Invalid Input!\n");
+                        break;
+                    }
+                  } else {
+                    system("clear");
+                    printf("Transfer failed, insufficient balance\n\n");
+                    printf("1. Back\n");
+                    printf("Selection (1) : ");
+                    scanf("%d", &inputMenu);
+                    switch (inputMenu){
+                      case 1:
+                        menu = 1;
+                        break;
+                      default:
+                        printf("Invalid Input!\n");
+                        break;
                     }
                   }
-
-                  if(transferToId != -1){
-                    transactionHistory[transactionHistoryLabel].nominal = transferNominal;
-                    user[activeUser].balance -= transferNominal;
-                    user[transferToId].balance += transferNominal;
-                    transactionHistoryLabel++;
-                    printf("Transfer Success\n\n");
-                  } else {
-                    printf("Transfer Failed, Invalid Target Card Id\n\n");
-                  }
-
-                  printf("1. Back\n");
-                  printf("0. Close & logout\n");
-                  scanf("%d", &inputMenu);
-                  switch (inputMenu){
-                    case 1:
-                      menu = 1;
-                      break;
-                    case 0:
-                      menu = 0;
-                      system("clear");
-                      break;
-                    default:
-                      printf("Invalid Input!");
-                      break;
-                  }
-                } else {
-                  system("clear");
-                  printf("Transfer failed, insufficient balance\n\n");
-                  printf("1. Back\n");
-                  printf("0. Close & logout\n");
-                  scanf("%d", &inputMenu);
-                  switch (inputMenu){
-                    case 1:
-                      menu = 1;
-                      break;
-                    case 0:
-                      menu = 0;
-                      system("clear");
-                      break;
-                    default:
-                      printf("Invalid Input!");
-                      break;
-                  }
-                }
-                break;
-              default:
-                printf("Invalid Input transaction!");
-                break;
-            }
-            break;
-          case 3:
-            // print transaction by user
-            system("clear");
-            printf("ID Card\t: %s \n", user[activeUser].idCard);
-            printf("Balance\t: $%d \n\n", user[activeUser].balance);
-            int hasTransaction = 0;
-            for (int i = 0; i < 64; i++)
-            {
-              if( strcmp(transactionHistory[i].from, user[activeUser].idCard) == 0 || strcmp(transactionHistory[i].to, user[activeUser].idCard) == 0 ){
-                printf("\nTransaction\n");
-                printf("from\t: %s \n", transactionHistory[i].from);
-                printf("to\t: %s \n", transactionHistory[i].to);
-                printf("nominal\t: $%d\n", transactionHistory[i].nominal);
-                hasTransaction = 1;
+                  break;
+                default:
+                  printf("Invalid Input transaction!");
+                  break;
               }
-            }
+              break;
+            case 3:
+              // print transaction by user
+              system("clear");
+              printf("ID Card\t: %s \n", user[activeUser].idCard);
+              printf("Balance\t: $%d \n\n", user[activeUser].balance);
+              int hasTransaction = 0;
+              for (int i = 0; i < 64; i++)
+              {
+                if( strcmp(transactionHistory[i].from, user[activeUser].idCard) == 0 || strcmp(transactionHistory[i].to, user[activeUser].idCard) == 0 ){
+                  printf("\nTransaction\n");
+                  printf("from\t: %s \n", transactionHistory[i].from);
+                  printf("to\t: %s \n", transactionHistory[i].to);
+                  printf("nominal\t: $%d\n", transactionHistory[i].nominal);
+                  hasTransaction = 1;
+                }
+              }
 
-            if(hasTransaction == 0){
-              printf("You don't have a transaction history...\n");
-            }
-            puts("");
-            printf("1. Back\n");
-            printf("0. Close & logout\n");
-            printf("Selection (1 or 0) : ");
-            scanf("%d", &inputMenu);
+              if(hasTransaction == 0){
+                printf("You don't have a transaction history...\n");
+              }
+              puts("");
+              printf("1. Back\n");
+              printf("Selection (1) : ");
+              scanf("%d", &inputMenu);
 
-            switch (inputMenu){
-              case 1:
-                menu = 1;
-                break;
-              case 0:
-                menu = 0;
-                system("clear");
-                break;
-              default:
-                printf("Invalid Input!");
-                break;
-            }
-            break;
-          default:
-            break;
+              switch (inputMenu){
+                case 1:
+                  menu = 1;
+                  break;
+                default:
+                  printf("Invalid Input!\n");
+                  break;
+              }
+              break;
+
+            case 0:
+              menu = 0;
+              isLogout = 1;
+              system("clear");
+              break;
+            default:
+              printf("Invalid Input!\n");
+              break;
+          }
         }
-      }
-  }
-  else
-  {
-    printf("\nLogin Failed\n\n");
-  }
-  // end | show status user
+    }
+    else
+    {
+      printf("\nLogin Failed\n\n");
+    }
+    // end | show status user
+  } while (isLogout == 1);
 }
